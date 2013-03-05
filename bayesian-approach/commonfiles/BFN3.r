af9f5fab1ecc07c3773d8bdc95a3abd6e47ec0d1
shanmukhasampath <shanmukhasampath@gmail.com>
2013-03-05 13:53:04 +0000

    Update BFN3.r

--- bayesian-approach/commonfiles/BFN3.r
+++ bayesian-approach/commonfiles/BFN3.r
@@ -1,9 +1,11 @@
+#BAYES FACTOR CALCULATOR FOR 3-SELECITVE CASE
 BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 {
   cutval <- cutvalue
 	nrprobesets<-nrow(DATA)
 	nrarrays<-ncol(DATA)
 	nrconditions<-length(nrreplics)
+	#TOP 3 HIGHLY EXPRESSED TISSUES FOR EACH PROBESET IS CONSIDERED
 	target <- matrix(rep(0,nrprobesets*3),nrow=nrprobesets,ncol=3)
 
 	#Preliminary: Calculation of condition-specific averages for all probesets
@@ -77,15 +79,11 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 	indicsum<-rep(0,nrprobesets)
 	NRREPLICS<-rep(1,nrprobesets)%*%t(nrreplics)
 	MU_SAMPLED2<-matrix(rep(0,nrprobesets*nrarrays),nrow=nrprobesets)
-	#SIGNDIFF <- matrix(rep(0,nrprobesets*(nrconditions-3)),nrow=nrprobesets,ncol=(nrconditions-3))
 	SIGNDIFF <- matrix(rep(0,nrprobesets),nrow=nrprobesets)
 	for (i in (1:nrruns)){
-        #cat('Average: \n',AV2,'\n')
-    		SEM<-sqrt(SIGMA2_SAMPLED/NRREPLICS)
-    		#cat('Standard Deviation for run ',i,': \n',SEM,'\n')
+        	SEM<-sqrt(SIGMA2_SAMPLED/NRREPLICS)
     		MU_SAMPLED<-AV2+(SEM*matrix(rnorm(nrprobesets*nrconditions),nrow=nrprobesets))
-    		if(nrruns == 1){
-        }
+    		if(nrruns == 1){}
         else{
              for(k in 1:nrprobesets){
                    if((target[k,1] > target[k,2]) && (target[k,2] > target[k,3])){
@@ -133,15 +131,13 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
               }
              s_d <- sd(t(AV3),na.rm=FALSE)
         }
-    		#cat('Samples Mean per tissue for run ',i,': \n',MU_SAMPLED,'\n')
     		lower<-1
     		for (j in (1:nrconditions)){
         		upper<-lower+nrreplics[j]-1
         		MU_SAMPLED2[,lower:upper]<-MU_SAMPLED[,j]%*%t(rep(1,nrreplics[j]))
         		lower<-upper+1
     		}
-    		#cat('Samples Mean tissue with replications for run ',i,': \n',MU_SAMPLED2,'\n')
-
+    		
     TEMP_MU_SAMPLED <- MU_SAMPLED
     for(k in (1:nrprobesets)){
         if((target[k,1] > target[k,2]) && (target[k,2] > target[k,3])){
@@ -151,7 +147,6 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 			      TEMP2 <- t(TEMP2)
 			      TEMP3 <- TEMP2[1,-(target[k,3])]
 			      TEMP3 <- t(TEMP3)
-    	      #SIGNDIFF[k,] <-updown*sign((MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k]))%*%t(rep(1,nrconditions-3)) - TEMP3)
     	      SIGNDIFF[k] <- updown*sign(MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k] + max(TEMP3)[1]))
     	  }
        else if((target[k,1] < target[k,2]) && (target[k,2] < target[k,3])){
@@ -161,7 +156,6 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 			      TEMP2 <- t(TEMP2)
 			      TEMP3 <- TEMP2[1,-(target[k,1])]
 			      TEMP3 <- t(TEMP3)
-    	      #SIGNDIFF[k,] <-updown*sign((MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k]))%*%t(rep(1,nrconditions-3)) - TEMP3)
     	      SIGNDIFF[k] <- updown*sign(MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k] + max(TEMP3)[1]))
         }
        else if((target[k,1] > target[k,2]) && (target[k,2] < target[k,3]) && (target[k,1] > target[k,3])){
@@ -171,7 +165,6 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 			      TEMP2 <- t(TEMP2)
 			      TEMP3 <- TEMP2[1,-(target[k,2])]
 			      TEMP3 <- t(TEMP3)
-    	      #SIGNDIFF[k,] <-updown*sign((MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k]))%*%t(rep(1,nrconditions-3)) - TEMP3)
     	      SIGNDIFF[k] <- updown*sign(MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k] + max(TEMP3)[1]))
         }
        else if((target[k,1] > target[k,2]) && (target[k,2] < target[k,3]) && (target[k,1] < target[k,3])){
@@ -181,7 +174,6 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 			      TEMP2 <- t(TEMP2)
 			      TEMP3 <- TEMP2[1,-(target[k,2])]
 			      TEMP3 <- t(TEMP3)
-    	      #SIGNDIFF[k,] <-updown*sign((MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k]))%*%t(rep(1,nrconditions-3)) - TEMP3)
     	      SIGNDIFF[k] <- updown*sign(MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k] + max(TEMP3)[1]))
         }
        else if((target[k,1] < target[k,2]) && (target[k,2] > target[k,3]) && (target[k,1] > target[k,3])){
@@ -191,7 +183,6 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 			      TEMP2 <- t(TEMP2)
 			      TEMP3 <- TEMP2[1,-(target[k,3])]
 			      TEMP3 <- t(TEMP3)
-    	      #SIGNDIFF[k,] <-updown*sign((MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k]))%*%t(rep(1,nrconditions-3)) - TEMP3)
     	      SIGNDIFF[k] <- updown*sign(MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k] + max(TEMP3)[1]))
         }
         else{
@@ -201,30 +192,19 @@ BFN3<-function(DATA,nrreplics,updown,nrruns,cutvalue)
 			      TEMP2 <- t(TEMP2)
 			      TEMP3 <- TEMP2[1,-(target[k,1])]
 			      TEMP3 <- t(TEMP3)
-    	      #SIGNDIFF[k,] <-updown*sign((MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k]))%*%t(rep(1,nrconditions-3)) - TEMP3)
     	      SIGNDIFF[k] <- updown*sign(MU_SAMPLED[k,target[k,3]] - (cutval * s_d[k] + max(TEMP3)[1]))
         }
-  	}
-		#SIGNDIFF<-updown*sign((MU_SAMPLED[,target[1]] - MU_SAMPLED[,target[2]])%*%t(rep(1,nrconditions))-MU_SAMPLED)
-		#cat('Signdifference for run ',i,': \n',SIGNDIFF,'\n')
-		
-    #indic<-rowSums(SIGNDIFF,2)==(nrconditions-3)
+    }		
     indic <- (SIGNDIFF) == 1
-		indicsum<-indicsum+indic
-		#cat('Indicsum for run ',i,': \n',indicsum,'\n')
-		#cat('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%','\n')
-		#df<-nrarrays-3
-		df<-nrarrays-2
+    indicsum<-indicsum+indic
+    df<-nrarrays-2
     nscale<-rowSums((MU_SAMPLED2-DATA)^2)
     chisqvals<-2*rgamma(nrprobesets, df/2, rate = 1, scale = 1)
-   	sigma2_sampled<-nscale*chisqvals^(-1)
+    sigma2_sampled<-nscale*chisqvals^(-1)
     SIGMA2_SAMPLED<-sigma2_sampled%*%t(rep(1,nrconditions))
-	}
-	#print(SIGNDIFF)
-  #print(target)
-  #print(indicsum)
+   }
+	
 	f1<-indicsum/nrruns
 	f2<-1-f1
-  #BFN3<-nrconditions*f1/((nrconditions/(nrconditions-3))*f2)
-	BFN3<- (((nrconditions)^2+2)*(nrconditions-3) * f1) / (6*f2)
+  	BFN3<- (((nrconditions)^2+2)*(nrconditions-3) * f1) / (6*f2)
 }
