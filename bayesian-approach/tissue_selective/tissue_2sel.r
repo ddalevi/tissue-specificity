40e942b28c69107971cc6aee1a9a6cbc441dd19e
shanmukha sampath <shanmukhasampath@gmail.com>
2013-03-07 10:52:55 +0000

    UPDATE

--- bayesian-approach/tissue_selective/tissue_2sel.r
+++ bayesian-approach/tissue_selective/tissue_2sel.r
@@ -1,3 +1,4 @@
+#CALCULATES THE OVERLAP OF 2-SELECTIVE GENES ACROSS FIVE DATASETS.
 source("../commonfiles/RMDUP.r") #EDIT THE DIRECTORY PATH
 source("../commonfiles/RMDUP1.r") #EDIT THE DIRECTORY PATH
 source("../commonfiles/OVP.r") #EDIT THE DIRECTORY PATH
@@ -6,7 +7,7 @@ h596 <- as.matrix(read.table("h596_2sel.txt"))
 h7307 <- as.matrix(read.table("h7307_2sel.txt"))
 m9954 <- as.matrix(read.table("m9954_2sel.txt"))
 r952 <- as.matrix(read.table("r952_2sel.txt"))
-
+#STORING THE OVERLAP OF 2-SELECTIVE GENES IN 5 DIMENSIONAL MATRIX FOR FIVE DATASETS
 sel2_overlap <- matrix(rep(0,5*5),nrow=5,ncol=5)
 
 h3113_2sel <- RMDUP(h3113)
@@ -16,6 +17,7 @@ m9954_2sel <- RMDUP(m9954)
 r952_2sel <- RMDUP(r952)
 
 temp <- list(h3113_2sel,h596_2sel,h7307_2sel,m9954_2sel,r952_2sel)
+#THE LOWER DIAGNOL OF MATRIX IS FILLED WITH OVERLAP BETWEEN DATASETS
 for(i in 1:nrow(sel2_overlap)){
       for(j in 1:ncol(sel2_overlap)){
             #if(i!=j && (i > j)){
@@ -31,7 +33,7 @@ for(i in 1:nrow(sel2_overlap)){
             }
       }
 }
-
+#THE UPPER DIAGONAL OF MATRIX IS FILLED WITH PERCENTAGE OVERLAP BETWEEN DATASETS
 for(i in 1:nrow(sel2_overlap)){
       for(j in 1:ncol(sel2_overlap)){
             if(i < j){
@@ -45,7 +47,7 @@ temp1 <- rbind(h3113_2sel,h596_2sel,h7307_2sel,m9954_2sel,r952_2sel)
 temp2 <- RMDUP1(temp1)
 temp1 <- temp2
 rm(temp2)
-
+#CLUSTERING OF THE DATASETS
 sel2_clust <- matrix(rep(0,length(temp1)*5),nrow=length(temp1),ncol=5)
 row.names(sel2_clust) <- temp1
 colnames(sel2_clust) <- c("Human(GDS3113)","Human(GDS596)","Human(GSE7307)","Mouse(GSE9954)","Rat(GSE952)")
